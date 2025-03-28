14daadc21679f5f4ec68d8d7ee5fbe09476606e7
AbrarShakilAhmed <skshakilppki123@gmail.com>
2025-03-28 21:00:37 +0600

    doing

--- index.php
+++ index.php
@@ -3,16 +3,19 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Php</title>
+  <title>Personal login Site</title>
+  <link rel="stylesheet" href="style.css">
+  <link rel="shortcut icon" href="./Place Your Image.png" type="image/x-icon">
 </head>
 <body>
   <div>
     <form action="form.php" method="get">
-      <label for="name">Name</label>
+      <h1>Login</h1>
+      <label for="name">Name :</label>
       <input type="text" name="name">
-      <label for="email">Email</label>
+      <label for="email">Email :</label>
       <input type="email" name="email" id="email">
-      <label for="Password">Password</label>
+      <label for="Password">Password :</label>
       <input type="password" name="pass" id="Password">
       <input type="submit" value="submit">
     </form>
