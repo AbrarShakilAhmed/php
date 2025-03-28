a1c7ce8cf1c32ccb6ebdb3406f5a94ef172dbd4f
AbrarShakilAhmed <skshakilppki123@gmail.com>
2025-03-28 20:44:03 +0600

    practicing

--- index.php
+++ index.php
@@ -10,6 +10,10 @@
     <form action="form.php" method="get">
       <label for="name">Name</label>
       <input type="text" name="name">
+      <label for="email">Email</label>
+      <input type="email" name="email" id="email">
+      <label for="Password">Password</label>
+      <input type="password" name="pass" id="Password">
       <input type="submit" value="submit">
     </form>
   </div>
