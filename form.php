e6f290fd72fb0a5b8c0be03eabf2896f108e932c
AbrarShakilAhmed <skshakilppki123@gmail.com>
2025-03-29 12:26:07 +0600

    done

--- form.php
+++ form.php
@@ -2,6 +2,35 @@
   $name =$_POST["name"];
   $email = $_POST["email"];
   $pass =$_POST["pass"];
+
+$nameSvd = 'Abrar Shakil';
+$emailSvd = "skshakilppki123@gmail.com";
+$passSvd = "hacker202";
+$hashed_password = '$6$rounds=1000000$NJy4rIPjpOaU$0ACEYGg/aKCY3v8O8AfyiO7CTfZQ8/W231Qfh2tRLmfdvFD6XfHk12u6hMr9cYIA4hnpjLNSTRtUwYr9km9Ij/';
+
+
+$crtNameHash ='$6$rounds=1000000$NJy4rIPjpOaU$ZzVfunJnDQyi0Jz3HSs3LZXcbX85xCM5t264l7/ov9C1z3KDs5ntMU136GQm6dKPOS8p/tw0yYTGpZeOfQozs/';
+
+$crtPassHash ='$6$rounds=1000000$NJy4rIPjpOaU$0dqjdufHu/zIBTXvgoWRcQceYzWmPBFErciyIdIeGF8L.JCg85bqBtK6.B5B8bqmtoU7Tr7nW46w9hhK3wMR9/';
+
+$crtEmailHash ='$6$rounds=1000000$NJy4rIPjpOaU$fDAFH4KR7W.AhDoh8WjlvUzYgPMU30YvCLR67N9ga5AEodPbWCHsr9HAiR0KxOlEygL09A5UYXBQSe3sBVAoR0';
+
+
+
+
+if(hash_equals($crtNameHash,crypt($name,$hashed_password))){
+  if(hash_equals($crtEmailHash,crypt($email,$hashed_password))){
+    if(hash_equals($crtPassHash,crypt($pass,$hashed_password))){
+      echo " <h1>You have loged in</h1>";
+    }else{
+      echo "pass is wrong <script>alert('Password is not matched')</script>";
+    }
+  }else{
+    echo "email is wrong <script>alert('email is not matched')</script>";
+  }
+}else{
+  echo "name is wrong <script>alert('name is not matched')</script>";
+}
   ?>
 <!DOCTYPE html>
 <html lang="en">
